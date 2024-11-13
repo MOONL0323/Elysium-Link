526035727d89b8a1314c542627a760dc29c62d0c
MOONL <moonlki@outlook.com>
2024-11-13 16:29:26 +0800

    base-template

---
+++ src/main/java/com/example/elysiumlink/ElysiumLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.elysiumlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElysiumLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elysiumlink/ElysiumLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elysiumlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElysiumLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
